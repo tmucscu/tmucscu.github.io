324be9799b2500f9f9e6794049b4a6f5079b622c
Jason Truong <71723813+jasonntruong@users.noreply.github.com>
2022-08-14 00:13:20 -0400

    change deployment branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "tmucscu", // Usually your GitHub org/user name.
   projectName: "tmucscu.github.io", // Usually your repo name.
-  deploymentBranch: "main",
+  deploymentBranch: "deployment",
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
