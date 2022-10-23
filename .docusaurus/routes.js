2f8f42047678d6688d1b7d01b493841cff0b297a
Jason Truong <71723813+jasonntruong@users.noreply.github.com>
2022-10-23 18:01:40 -0400

    merge

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -1,68 +1,40 @@
-import React from 'react';
 import ComponentCreator from '@docusaurus/ComponentCreator';
+import React from 'react';
 
 export default [
   {
     path: '/__docusaurus/debug',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug', '9d6'),
-=======
     component: ComponentCreator('/__docusaurus/debug', '08c'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/config',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/config', '4ed'),
-=======
     component: ComponentCreator('/__docusaurus/debug/config', '97f'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/content',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/content', 'cea'),
-=======
     component: ComponentCreator('/__docusaurus/debug/content', '902'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/globalData',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/globalData', '15d'),
-=======
     component: ComponentCreator('/__docusaurus/debug/globalData', '358'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/metadata',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/metadata', '9cc'),
-=======
     component: ComponentCreator('/__docusaurus/debug/metadata', '49d'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/registry',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/registry', '072'),
-=======
     component: ComponentCreator('/__docusaurus/debug/registry', '4b4'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
     path: '/__docusaurus/debug/routes',
-<<<<<<< HEAD
-    component: ComponentCreator('/__docusaurus/debug/routes', '3bb'),
-=======
     component: ComponentCreator('/__docusaurus/debug/routes', 'b3d'),
->>>>>>> ad8d42b1e68ec155e502d499dc804a7f20fc67d8
     exact: true
   },
   {
